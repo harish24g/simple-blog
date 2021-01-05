e19d2500786ffe4258d2f882c7f19631df0ddf11
Harish G <cr7rko666@gmail.com>
2021-01-05 23:29:25 +0530

    few changes

--- myblog.py
+++ myblog.py
@@ -37,13 +37,13 @@ posts = [
         'author': 'Harish',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'JAN 4, 2021'
     },
     {
         'author': 'Harold Finch',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'JAN 5, 2021'
     }
 ]
 
